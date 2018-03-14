1004375a17e90d50bb16268cc50111b9080ff109
sylvie zhang <sylvie@sylvies-MacBook-Pro.local>
2018-03-14 22:54:57 +0100

    modif join

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,29 @@
 #include "get_next_line.h"
 
-static int		ft_read_file(char **str, int fd)
+/*static char		*ft_join(char *s1, const char *s2)
+{
+	char	*tmp;
+
+	if (!(tmp = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
+		return (NULL);
+	ft_strcpy(tmp, s1);
+	free(s1);
+	ft_strcat(tmp, s2);
+	return (tmp);
+}*/
+
+static	char	*ft_join(char const *s1, char const *s2)
+{
+	char	*tmp;
+
+	if ((tmp = ft_strnew((size_t)ft_strlen(s1) + ft_strlen(s2))) == NULL)
+		return (tmp);
+	ft_strcpy(tmp, s1);
+	ft_strcat(tmp, s2);
+	return (tmp);
+}
+
+static	int		ft_read_file(char **str, int fd)
 {
 	int		ret;
 	char	*s;
@@ -10,13 +33,13 @@ static int		ft_read_file(char **str, int fd)
 		return (-1);
 	buf[ret] = '\0';
 	s = *str;
-	*str = ft_strjoin(*str, buf);
+	*str = ft_join(*str, buf);
 	if (*s != 0)
 		free(s);
 	return (ret);
 }
 
-static int		ft_gl(char **str, char **line, char *s)
+static	int		ft_gl(char **str, char **line, char *s)
 {
 	int		lines;;
 	char	*joinstr;
@@ -25,7 +48,7 @@ static int		ft_gl(char **str, char **line, char *s)
 	if (*s == '\n')
 		lines = 1;
 	*s = 0;
-	*line = ft_strjoin("", *str);
+	*line = ft_join("", *str);
 	if (lines == 0 && ft_strlen(*str) != 0)
 	{
 		*str = ft_strnew(1);
@@ -34,7 +57,7 @@ static int		ft_gl(char **str, char **line, char *s)
 	else if (lines == 0 && !(ft_strlen(*str)))
 		return (0);
 	joinstr = *str;
-	*str = ft_strjoin(s + 1, "");
+	*str = ft_join(s + 1, "");
 	free(joinstr);
 	return (lines);
 }
@@ -47,7 +70,7 @@ int				get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
-	if (str == 0)
+	if (str == '\0')
 		str = "";
 	ret = BUFF_SIZE;
 	while (line)
@@ -55,7 +78,7 @@ int				get_next_line(const int fd, char **line)
 		s = str;
 		while (*s || ret < BUFF_SIZE)
 		{
-			if (*s == '\n' || *s == 0 || *s == -1)
+			if (*s == '\n' ||*s == '\0' || *s == -1)
 				return (ft_gl(&str, line, s));
 			s++;
 		}
