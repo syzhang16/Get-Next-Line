0b0ac7071aa105014702f5f3bab73022be0e505e
Zhang Sylvie <syzhang@student.42.fr>
2018-03-16 16:52:45 +0100

    modify

--- get_next_line.c
+++ get_next_line.c
@@ -1,17 +1,5 @@
 #include "get_next_line.h"
 
-/*static char		*ft_join(char *s1, const char *s2)
-{
-	char	*tmp;
-
-	if (!(tmp = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
-		return (NULL);
-	ft_strcpy(tmp, s1);
-	free(s1);
-	ft_strcat(tmp, s2);
-	return (tmp);
-}*/
-
 static	char	*ft_join(char const *s1, char const *s2)
 {
 	char	*tmp;
@@ -41,25 +29,25 @@ static	int		ft_read_file(char **str, int fd)
 
 static	int		ft_gl(char **str, char **line, char *s)
 {
-	int		lines;;
+	int		line_index;
 	char	*joinstr;
 
-	lines = 0;
+	line_index = 0;
 	if (*s == '\n')
-		lines = 1;
+		line_index = 1;
 	*s = '\0';
 	*line = ft_join("", *str);
-	if (lines == 0 && ft_strlen(*str) != 0)
+	if (line_index == 0 && ft_strlen(*str) != 0)
 	{
 		*str = ft_strnew(1);
 		return (1);
 	}
-	else if (lines == 0 && !(ft_strlen(*str)))
+	else if (line_index == 0 && !(ft_strlen(*str)))
 		return (0);
 	joinstr = *str;
 	*str = ft_join(s + 1, "");
 	free(joinstr);
-	return (lines);
+	return (line_index);
 }
 
 int				get_next_line(const int fd, char **line)
@@ -72,13 +60,13 @@ int				get_next_line(const int fd, char **line)
 		return (-1);
 	if (str == '\0')
 		str = "";
-	ret = BUFF_SIZE;
+	ret = BUFF_SIZE + 1;
 	while (line)
 	{
 		s = str;
 		while (*s || ret < BUFF_SIZE)
 		{
-			if (*s == '\n' ||*s == '\0' || *s == -1)
+			if (*s == '\n' || *s == '\0')
 				return (ft_gl(&str, line, s));
 			s++;
 		}
