f6958daa5aca3d17b5fa18dbf6f4b4614664fa98
sylvie zhang <sylvie@sylvies-MacBook-Pro.local>
2018-03-27 23:14:00 +0200

    ajout header

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzhang <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/27 15:02:12 by syzhang           #+#    #+#             */
+/*   Updated: 2018/03/27 15:02:21 by syzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -6,7 +18,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFF_SIZE 7
+# define BUFF_SIZE 1
 
 int		get_next_line(const int fd, char **line);
 
